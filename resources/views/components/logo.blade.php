ccd6a835cbe475d3b01e447c906d210b5638053a
Wojciech Przybyła T <contact@wpww.pl>
2023-01-10 23:05:11 +0100

    Icon!

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ Auth::check() ? "/dashboard" : "/" }}">
-    <img src="{{ URL::asset("msznm.svg") }}" alt="Logo" class="logo">
+    <img src="{{ URL::asset("icon.svg") }}" alt="Logo" class="logo">
 </a>
