006c2b0cf116f28f0cde2781f3ac0eea34ad898f
Wojciech Przybyła T <contact@wpww.pl>
2023-03-02 22:16:02 +0100

    Aaa, recipe conflict

--- resources/views/components/amount.blade.php
+++ resources/views/components/amount.blade.php
@@ -1,6 +1,6 @@
 <span
     @if (!$template && $ingredient->template?->minimum_amount > $ingredient->amount) class="error" @endif
-    @if ($ingredient->template->unit === "JNO")
+    @if (($ingredient->template?->unit) ?? $ingredient->unit === "JNO")
     {{ Popper::pop($ingredient->amount) }}
     @endif
     >
